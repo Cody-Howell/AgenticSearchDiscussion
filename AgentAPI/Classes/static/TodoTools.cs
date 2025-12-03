72cf217e688863bc27d13a11acbe216b87c53007
Cody <codyhowell44@gmail.com>
2025-12-02 17:25:19 -0700

    more functions and system prompts

--- AgentAPI/Classes/static/TodoTools.cs
+++ AgentAPI/Classes/static/TodoTools.cs
@@ -21,4 +21,57 @@ public static class TodoTools {
             }
         }
     };
+
+    public static Tool GetTopLevelFoldersTool() => new Tool {
+        Type = "function",
+        Function = new CalledFunction {
+            Name = "get_top_level_folders",
+            Description = "Retrieve the list of top-level folders in the data repository.",
+            Parameters = new Parameters {
+                Type = "object",
+                Properties = new Dictionary<string, ParameterDescription>(),
+                ParametersRequired = []
+            }
+        }
+    };
+
+    public static Tool GetFilesInFolderTool() => new Tool {
+        Type = "function",
+        Function = new CalledFunction {
+            Name = "get_files_in_folder",
+            Description = "Retrieve the list of file paths in a specific folder within the data repository.",
+            Parameters = new Parameters {
+                Type = "object",
+                Properties = new Dictionary<string, ParameterDescription> {
+                    ["folder"] = new ParameterDescription {
+                        Type = "string",
+                        Description = "The name of the folder to retrieve files from."
+                    },
+                },
+                ParametersRequired = ["folder"]
+            }
+        }
+    };
+
+    public static Tool GetFileContentsTool() => new Tool {
+        Type = "function",
+        Function = new CalledFunction {
+            Name = "get_file_contents",
+            Description = "Retrieve the contents of a specific file within a folder in the data repository.",
+            Parameters = new Parameters {
+                Type = "object",
+                Properties = new Dictionary<string, ParameterDescription> {
+                    ["folder"] = new ParameterDescription {
+                        Type = "string",
+                        Description = "The name of the folder containing the file."
+                    },
+                    ["relpath"] = new ParameterDescription {
+                        Type = "string",
+                        Description = "The relative path to the file within the folder."
+                    },
+                },
+                ParametersRequired = ["folder", "relpath"]
+            }
+        }
+    };
 }
